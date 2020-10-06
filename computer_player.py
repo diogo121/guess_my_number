2c312d536987801a94b737605f63bffbb3986a83
diogo121 <72268893+diogo121@users.noreply.github.com>
2020-10-06 08:45:38 +0200

    Add files via upload

---
+++ computer_player.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct  5 15:52:42 2020
+
+@author: Diogo
+"""
+
+import random
+from guess_my_number import MIN, MAX, GuessMachine
+
+if __name__=="__main__":
+    min=MIN
+    max=MAX
+    guess_machine=GuessMachine()
+    while True:
+        attempt=int((min+max)/2)
+        result=guess_machine.guess(attempt)
+        print("Tried %d : %s" % (attempt, result))
+        if result == "found":
+            print("Finisched in %d attempts." % guess_machine.number_of_attempt)
+            break
+        elif result == "too low"
+            min=attempt +1
+        else:
+            max=attempt -1
